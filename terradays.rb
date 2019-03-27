52ab5c4aaf07b764026e3bbc8f56cf34a6ad67f1
Stefan Martinov <stefan.martinov@gmail.com>
2019-03-27 10:15:44 +0100

    Update terradays.rb

--- terradays.rb
+++ terradays.rb
@@ -1,7 +1,7 @@
 class Terradays < Formula
   desc "Terraform utility, cause even the good ones need a couple of days to create a planet"
   homepage "https://github.com/foomo/terradays"
-  url "https://github.com/foomo/terradays/releases/download/0.1.2/terradays_0.1.2_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/foomo/terradays/releases/download/0.1.2/terradays_0.1.2_darwin_amd64.tar.gz"
   version "0.1.2"
   sha256 "d4490b8b76619633367f25a850b5e7bc8b19d032957d305f3075a6f682838f50"
 
