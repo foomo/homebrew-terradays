44c30acbc1409810f1a60aeda12a83cb43091d0c
goreleaserbot <goreleaser@carlosbecker.com>
2019-10-22 22:07:22 +0200

    Brew formula update for terradays version 0.3.0

--- terradays.rb
+++ terradays.rb
@@ -1,9 +1,9 @@
 class Terradays < Formula
   desc "Terraform utility, cause even the good ones need a couple of days to create a planet"
   homepage "https://github.com/foomo/terradays"
-  url "https://github.com/foomo/terradays/releases/download/0.2.0/terradays_0.2.0_darwin_amd64.tar.gz"
-  version "0.2.0"
-  sha256 "96c7d637337d2842cc22be9e5cde520dd12eb08c253eccc9ea6d8eaa90bcb9e8"
+  url "https://github.com/foomo/terradays/releases/download/0.3.0/terradays_0.3.0_darwin_amd64.tar.gz"
+  version "0.3.0"
+  sha256 "e5808a96ec90ebfef9ca2a062fd097ea6e94caee7815d77b222833111aec35e1"
 
   def install
     bin.install "bin/terradays"
